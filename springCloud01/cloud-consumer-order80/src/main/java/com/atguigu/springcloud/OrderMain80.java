6c97e63f59d739e9f7dbf3d285341af00cbe6895
shaolixiang <shaolixiangmail@foxmail.com>
2022-01-12 21:59:10 +0800

    80消费者端口

---
+++ springCloud01/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -0,0 +1,15 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shaolixiang
+ * @create 2022-01-12 21:07
+ */
+@SpringBootApplication
+public class OrderMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderMain80.class,args);
+    }
+}
