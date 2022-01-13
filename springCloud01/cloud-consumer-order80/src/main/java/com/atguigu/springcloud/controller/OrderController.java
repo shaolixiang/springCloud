aa347720c0114efb8ba125436d292b9e910200d5
shaolixiang <shaolixiangmail@foxmail.com>
2022-01-13 21:05:37 +0800

    创建cloud-api-commons整合公共代码entities

--- springCloud01/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ springCloud01/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -5,7 +5,6 @@ import com.atguigu.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -23,7 +22,7 @@ public class OrderController {
     @Resource
     private RestTemplate restTemplate;
 
-    @PostMapping("/consumer/payment/create")
+    @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment){
         return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
     }
