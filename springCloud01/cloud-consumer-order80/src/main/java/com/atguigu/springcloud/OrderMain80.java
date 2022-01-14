b2c39d3742cbd956a010e64f3b8cf8fe9bc2c0b5
shaolixiang <shaolixiangmail@foxmail.com>
2022-01-14 20:52:09 +0800

    单机eureka

--- springCloud01/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
+++ springCloud01/cloud-consumer-order80/src/main/java/com/atguigu/springcloud/OrderMain80.java
@@ -2,12 +2,14 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author shaolixiang
  * @create 2022-01-12 21:07
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class,args);
