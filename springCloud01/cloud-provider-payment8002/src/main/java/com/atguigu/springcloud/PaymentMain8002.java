8d2179f6f68327b007beff2ebab54706b705cdd6
shaolixiang <shaolixiangmail@foxmail.com>
2022-01-17 22:59:17 +0800

    cloud-payment-service服务提供者集群以及@LoadBalanced负载均衡

---
+++ springCloud01/cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/PaymentMain8002.java
@@ -0,0 +1,17 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author shaolixiang
+ * @create 2021-12-14 11:01
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class PaymentMain8002 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8002.class,args);
+    }
+}
---
+++ springCloud01/cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,28 @@
+package com.atguigu.springcloud.service.impl;
+
+import com.atguigu.springcloud.dao.PaymentDao;
+import com.atguigu.springcloud.entities.Payment;
+import com.atguigu.springcloud.service.PaymentService;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+/**
+ * @author shaolixiang
+ * @create 2021-12-14 15:53
+ */
+@Service
+public class PaymentServiceImpl implements PaymentService {
+    @Resource
+    private PaymentDao paymentDao;
+
+    public int create(Payment payment) {
+        return paymentDao.create(payment);
+    }
+
+    public Payment getPaymentById(Long id) {
+        return paymentDao.getPaymentById(id);
+
+    }
+
+}
